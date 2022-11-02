30c2357ead0c7fdb5811cc4d792acf91446f731c
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2022-11-02 11:27:31 -0400

    add patch finding step #patch

--- .github/actions/github-tag-action/entrypoint.sh
+++ .github/actions/github-tag-action/entrypoint.sh
@@ -158,15 +158,16 @@ then
     # already a pre-release available, bump it
     newPreTagFmt="$new+(-$suffix\.[0-9]+)$"
     exists=git tag --list --merged HEAD --sort=-v:refname | grep -E "$newPreTagFmt" | head -n 1
+    echo -e "Found parent to ${new} pre-tag ${exists}..."
     if [[ $exists != "" ]]
     then
       if $with_v
-          then
-              new=v$(semver -i prerelease "${exists}" --preid "${suffix}")
-          else
-              new=$(semver -i prerelease "${exists}" --preid "${suffix}")
-          fi
-          echo -e "Bumping ${suffix} pre-tag ${exists}. New pre-tag ${new}"
+        then
+          new=v$(semver -i prerelease "${exists}" --preid "${suffix}")
+        else
+          new=$(semver -i prerelease "${exists}" --preid "${suffix}")
+      fi
+      echo -e "Bumping ${suffix} pre-tag ${exists}. New pre-tag ${new}"
     elif [[ "$pre_tag" =~ $new ]] && [[ "$pre_tag" =~ $suffix ]]
     then
         if $with_v
